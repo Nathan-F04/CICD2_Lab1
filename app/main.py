183417f46a60afd5818d6f51fcefe912764a4668
Nathan <G00413711@atu.ie>
2025-10-01 08:33:40 +0100

    removing hello world function due to code review note

--- app/main.py
+++ app/main.py
@@ -5,20 +5,17 @@ from .schemas import User
 app = FastAPI()
 users: list[User] = []
 
-@app.get("/hello")
-def hello():
-    return {"message": "Hello, World!"}
-
 @app.get("/api/users")
 def get_users():
     return users
 
-@app.get("/api/users/health")
+@app.get("/health")
 def health():
     return {"status": "ok"}
 
 @app.get("/api/users/{user_id}")
 def get_user(user_id: int):
+    #loop users until an id matches
     for u in users:
         if u.user_id == user_id:
             return u
@@ -26,26 +23,31 @@ def get_user(user_id: int):
     
 @app.post("/api/users", status_code=status.HTTP_201_CREATED)
 def add_user(user: User):
+    #checks the user object id matches for any object
     if any(u.user_id == user.user_id for u in users):
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="user_id already exists")
     users.append(user)
     return user
 
-@app.put("/api/users/", status_code=status.HTTP_201_CREATED)
-def update_user(user: User):
-    for u in users:
-        if(u.user_id == user.user_id):
-            users[users.index(u)] = user 
-            return user
+@app.put("/api/users/{user_id}")
+def edit_user(user_id: int, user: User):
+    #Compare the object at each index to see matching id
+    for i, u in enumerate(users):
+        if u.user_id == user_id:
+            #Once they match change the attributes at the index
+            users[i].name = user.name
+            users[i].email = user.email
+            users[i].age = user.age
+            users[i].student_id = user.student_id
+            return users[i]
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
 
 @app.delete("/api/users/{user_id}", status_code=204)
 def delete_user(user_id: int):
+    #Scans each user object in list
     for u in users:
         if(u.user_id == user_id):
+            #Upon matching id passed with user object id, the user is removed
             users.remove(u)
             return
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
-
-
-
